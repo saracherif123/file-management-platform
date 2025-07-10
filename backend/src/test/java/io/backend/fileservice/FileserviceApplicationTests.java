b8222a1eb9d7cea706073db6a4cb0559cd0392ca
Sara Saad <saracherifsaad@gmail.com>
2025-07-10 11:42:30 +0200

    Initial backend

---
+++ backend/src/test/java/io/backend/fileservice/FileserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.backend.fileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
